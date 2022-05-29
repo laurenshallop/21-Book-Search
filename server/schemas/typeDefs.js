ee6589469d2a081aa0fe105c3226f8a67f53b457
Lauren Lucio <laurenlucio@Laurens-MacBook-Pro.local>
2022-05-29 10:58:37 -0700

    add

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,7 +41,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(newBook: InputBook!): User
+    saveBook(newBook: bookInput!): User
     removeBook(bookId: String!): User
   }
 `
