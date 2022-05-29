1d48cb6b30030309d08b373b5f4c7550ab427d2f
Lauren Lucio <laurenlucio@Laurens-MacBook-Pro.local>
2022-05-29 13:21:47 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
